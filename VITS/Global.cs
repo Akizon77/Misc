191b1a978927c515090e724112ea8c93a12119c4
Akizon <hytf233g@gmail.com>
2023-06-15 22:11:21 +0800

    Vits

---
+++ VITS/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VITS
+{
+    public static class Global
+    {
+        public static string username = "3518634279";
+        public static string token = "8b445fff-f666-44eb-a19e-fed54ce7654e";
+    }
+}
