d002ad181f78f68f2a4c35c646f7de918839beab
Akizon <hytf233g@gmail.com>
2023-06-29 19:11:00 +0800

    中间件？

--- AkzMessager/Communicate/Models.cs
+++ AkzMessager/Communicate/Models.cs
@@ -6,8 +6,44 @@ namespace Communicate
     {
         public class Message
         {
+            public int Id { get; set; }//ID
+            public int Sender { get; set; }//发送消息的用户ID
+            public int Reci { get; set; }//接受消息的用户ID
+            public DateTime Date { get; set; }//时间
+            public Content Content { get; set; }//内容
+
+        }
+
+
+        public class Image
+        {
             public int Id { get; set; }
-            public DateTime data { get; set; }
+            public string Path { get; set; }
+
+
+        }
+
+        public class Session
+        {
+            public string Content { get; set; }
+        }
+        public class Content
+        {
+            public Image? picture;
+            public string? text;
+            Content(Image image)
+            {
+                picture = image;
+            }
+            Content(string text)
+            {
+                this.text = text;
+            }
+            Content(Image picture, string text)
+            {
+                this.picture = picture;
+                this.text = text;
+            }
         }
     }
 }
