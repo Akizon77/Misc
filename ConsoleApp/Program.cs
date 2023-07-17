e57df9e81f2b27c2933d643525907a0427a90022
Akizon <hytf233g@gmail.com>
2023-07-17 18:40:38 +0800

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,10 +10,16 @@ namespace Pro
             MyClass.Show();
             MyClass.Change(10);
             MyClass.Show();
+            var myClass1 = new MyClass2();
+            var myClass2 = new MyClass2();
+            var myClass3 = new MyClass2();
+            myClass2.Show();
+            //new 关键字：新建一个
+
         }
     }
     //封装了两个成员
-     static class MyClass
+    static class MyClass
     {
         //public(公共的) private(私有的) internal（内部的） 
         //Show() 是类的MyClass成员
@@ -27,6 +33,22 @@ namespace Pro
         }
         //int成员x
         private static int x = 5;
+
+    }
+    class MyClass2
+    {
+        //public(公共的) private(私有的) internal（内部的） 
+        //Show() 是类的MyClass成员
+         public void Show()
+        {
+            Console.WriteLine(x);
+        }
+         public void Change(int y)
+        {
+            x = y;
+        }
+        //int成员x
+        private  int x = 5;
 
     }
 }
