2ed4b3edfb1c67a342c1d906be360286e66ed37f
Akizon <hytf233g@gmail.com>
2023-07-26 11:30:00 +0800

    zzz

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,52 +4,25 @@ namespace Pro
 {
     class Program //类
     {
-        static void Main() //Main方法
+        private static int _a = 0;
+        public static int a
         {
-            int a;
-            MyClass.Show();
-            MyClass.Change(10);
-            MyClass.Show();
-            var myClass1 = new MyClass2();
-            var myClass2 = new MyClass2();
-            var myClass3 = new MyClass2();
-            myClass2.Show();
-            //new 关键字：新建一个
+            get { _a++; return _a; }
+            set { _a = value; }
+        }
+        static void Main()
+        {
+
+            if (a == 1 && a == 2 && a == 3)
+            {
+                Console.WriteLine(true);
+            }
 
         }
-    }
-    //封装了两个成员
-    static class MyClass
-    {
-        //public(公共的) private(私有的) internal（内部的） 
-        //Show() 是类的MyClass成员
-        static public void Show()
-        {
-            Console.WriteLine(x);
-        }
-        static public void Change(int y)
-        {
-            x = y;
-        }
-        //int成员x
-        private static int x = 5;
+
 
     }
-    class MyClass2
-    {
-        //public(公共的) private(私有的) internal（内部的） 
-        //Show() 是类的MyClass成员
-         public void Show()
-        {
-            Console.WriteLine(x);
-        }
-         public void Change(int y)
-        {
-            x = y;
-        }
-        //int成员x
-        private  int x = 5;
 
-    }
+
 }
 
--- DDNS.CloudFlare/Program.cs
+++ DDNS.CloudFlare/Program.cs
@@ -1,25 +1,13 @@
 ﻿using DDNS.CloudFlare;
 using Newtonsoft.Json.Linq;
 
-
-
-
-
-
-
-
-
-
 var email =  Helpers.LoadConfig("email");
 var apiKey = Helpers.LoadConfig("apiKey");
 var domain = Helpers.LoadConfig("rootDomain");
 var secondLevelDomain = Helpers.LoadConfig("ddnsDomain");
-
 var useIPv6 = Convert.ToBoolean(Helpers.LoadConfig("useIPv6"));
-
 var restartTime = Convert.ToInt32(Helpers.LoadConfig("restartTime"));
 var autoRun = Convert.ToBoolean(Helpers.LoadConfig("autoRun"));
-
 while(true)
 {
     try
@@ -27,13 +15,9 @@ while(true)
         Console.WriteLine($"本次解析 {secondLevelDomain} 到本机地址");
         var ip = await Helpers.GetMyIPAddress(useIPv6);
         Console.WriteLine($"本机公网IP : {ip}");
-
         var zoneID = await Helpers.GetZoneID(email, apiKey, domain);
         var domainID = await Helpers.GetSecondLevelDomainID(email, apiKey, zoneID, secondLevelDomain);
         if (!String.IsNullOrEmpty(domainID)) Console.WriteLine($"找到域名 {secondLevelDomain} Token:{domainID}");
-
-
-
         await Helpers.ChangeIPAddress(email, apiKey, zoneID, domainID, ip, secondLevelDomain, useIPv6);
         if (autoRun)
         {
@@ -42,9 +26,5 @@ while(true)
         }
         else break;
     }
-    finally
-    {
-
-    }
-
+    finally { }
 }
