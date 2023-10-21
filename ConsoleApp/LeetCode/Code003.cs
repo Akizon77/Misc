e49371993bb0bfe1e0c314edc3bfb539eeba49da
Akizon <hytf233g@gmail.com>
2023-10-21 15:38:04 +0800

    LeetCode 003 字符串长度

---
+++ ConsoleApp/LeetCode/Code003.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.LeetCode
+{
+    public static class Code003
+    {
+        public static void Run(string s)
+        {
+            var charlist = s.ToCharArray();
+            var originLenth = s.Length;//原始字符长度
+            var maxLenth = 0;//最大长度
+            var start = 0;//指针位置1：起始标记
+            var end = 0;//指针位置2：结束标记
+            var resultStr = new List<char>();
+            while (end < originLenth)//在指向最后一个字符之前
+            {
+                if (!resultStr.Contains(charlist[end]))//如果不包含end指向的当前字符
+                {
+                    resultStr.Add(charlist[end]);//添加进入列表
+                    end++;//移向下一位
+                    maxLenth = maxLenth > end - start ? maxLenth : end - start;//取end - start
+                }
+                else
+                {
+                    resultStr.Remove(charlist[start]);//移除起始字符
+                    start++;//移向下一位
+                }
+            }
+            Console.WriteLine(maxLenth);
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ConsoleApp.LeetCode;
+using System.Text;
 
 namespace P
 {
@@ -6,42 +7,7 @@ namespace P
     {
         static void Main()
         {
-            try
-            {
-                string? path;
-                while (true)
-                {
-                    Console.WriteLine("将文件夹拖拽到控制台内(或粘贴路径)，并按下回车");
-                    path = Console.ReadLine();
-                    if (Directory.Exists(path))
-                        break;
-                    Console.WriteLine("文件夹不存在！");
-                }
-                Console.WriteLine("开始将UTF8转化为GB2312");
-                var codeing = CodePagesEncodingProvider.Instance;
-                //codeing
-                //    .GetEncodings()
-                //    .ToList()
-                //    .ForEach(x => Console.WriteLine($"{x.CodePage} {x.DisplayName}"));
-
-                var files = Directory.GetFiles(path);
-
-                foreach (var file in files)
-                {
-                    Console.Write(file);
-                    var content = File.ReadAllText(file, Encoding.UTF8);
-                    File.WriteAllText(file, content, codeing.GetEncoding(936));
-                    Console.WriteLine("\t转化成功");
-                }
-                Console.WriteLine("转化完成，窗口5秒后关闭...");
-                Thread.Sleep(5000);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"出现异常");
-                Console.WriteLine(ex.Message);
-                Thread.Sleep(new TimeSpan(0,0,30));
-            }
+            Code003.Run("pwwkew");
         }
     }
 }
